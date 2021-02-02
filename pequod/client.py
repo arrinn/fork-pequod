a99a76333dc375d05566c403800efed9df69ee6b
Lev Khoroshansky <sololev27@gmail.com>
2021-02-02 14:45:30 +0000

    Pequod: client for Docker container

---
+++ pequod/client.py
@@ -0,0 +1,124 @@
+import os
+
+from typing import List
+
+from pequod.helpers import (
+    ensure_not_running_as_root,
+    group_id,
+    log,
+    run,
+    user_id,
+)
+from pequod.config import (
+    COURSE_NAME,
+    CONTAINER_NAME,
+    CONTAINER_PORT,
+    HOST_WORKSPACE_DIR,
+    HOST_DOCKER_COMPOSE_PATH,
+    CONTAINER_WORKSPACE_DIR,
+    CONTAINER_BASHRC_PATH,
+)
+
+
+class Client:
+
+    def __init__(self) -> None:
+        self._setup_env()
+
+    def _setup_env(self) -> None:
+        os.environ["COURSE_NAME"] = str(COURSE_NAME)
+        os.environ["CONTAINER_NAME"] = str(CONTAINER_NAME)
+        os.environ["CONTAINER_PORT"] = str(CONTAINER_PORT)
+        os.environ["HOST_WORKSPACE_DIR"] = str(HOST_WORKSPACE_DIR)
+
+    # --------------------------------------------------------------------------
+
+    @ensure_not_running_as_root
+    def login(self) -> None:
+        self._bash(user=f"{user_id()}:{group_id()}")
+
+    @ensure_not_running_as_root
+    def create(self) -> None:
+        log.info(f"Mounting from {HOST_WORKSPACE_DIR}")
+        self._compose_build()
+        self._add_group()
+        self._add_user()
+        self._change_owner()
+
+    def remove(self) -> None:
+        self._stop()
+        self._rm()
+
+    @ensure_not_running_as_root
+    def restart(self) -> None:
+        self._compose_restart()
+
+    @ensure_not_running_as_root
+    def root(self) -> None:
+        self._bash(user="root")
+
+    # --------------------------------------------------------------------------
+
+    def _bash(self, user: str) -> None:
+        cmd = [
+            "--user", f"{user}", "--env", "COURSE_NAME", f"{CONTAINER_NAME}",
+            "/bin/bash", "--rcfile", f"{CONTAINER_BASHRC_PATH}"
+        ]
+        self._exec(cmd)
+
+    # --------------------------------------------------------------------------
+
+    def _compose_build(self) -> None:
+        cmd = [
+            "docker-compose", "-f", f"{HOST_DOCKER_COMPOSE_PATH}", "up", "-d",
+            "--build", "--force-recreate"
+        ]
+        run(cmd)
+
+    def _add_group(self) -> None:
+        cmd = [f"{CONTAINER_NAME}", "groupadd", "-g", f"{group_id()}", "grp"]
+        self._exec(cmd, panic_on_error=False)
+
+    def _add_user(self) -> None:
+        user = os.environ["USER"]
+        cmd = [
+            f"{CONTAINER_NAME}", "useradd", "-u", f"{user_id()}", "-g",
+            f"{group_id()}", "-m", f"{user}"
+        ]
+        self._exec(cmd)
+
+    def _change_owner(self) -> None:
+        user = os.environ["USER"]
+        cmd = [
+            f"{CONTAINER_NAME}", "chown", "-R", f"{user}",
+            f"{CONTAINER_WORKSPACE_DIR}"
+        ]
+        self._exec(cmd)
+
+    # --------------------------------------------------------------------------
+
+    def _stop(self) -> None:
+        cmd = ["docker", "container", "stop", f"{CONTAINER_NAME}"]
+        run(cmd)
+
+    def _rm(self) -> None:
+        cmd = ["docker", "container", "rm", f"{CONTAINER_NAME}"]
+        run(cmd)
+
+    # --------------------------------------------------------------------------
+
+    def _compose_restart(self) -> None:
+        cmd = [
+            "docker-compose", "-f", f"{HOST_DOCKER_COMPOSE_PATH}", "up", "-d"
+        ]
+        run(cmd)
+
+    # --------------------------------------------------------------------------
+
+    def _exec(self, cmd: List[str], interactive: bool = True, **kwargs) -> None:
+        cmd = ["docker", "exec"] + (["-it"] if interactive else []) + cmd
+        run(cmd, **kwargs)
+
+    def _compose_config(self) -> None:
+        cmd = ["docker-compose", "config"]
+        run(cmd)
---
+++ pequod/helpers.py
@@ -0,0 +1,66 @@
+import os
+import sys
+import subprocess as sp
+
+from functools import wraps
+from typing import (
+    Any,
+    List,
+)
+from enum import (
+    Enum,
+    auto,
+)
+
+from pequod.logging import log
+
+
+class ErrorCode(Enum):
+    PRECONDITION_FAILED = auto()
+    CALLED_PROCESS_ERROR = auto()
+
+
+def run_as_root() -> bool:
+    return os.geteuid() == 0
+
+
+def panic(reason: str, code: ErrorCode) -> None:
+    log.critical(reason)
+    exit(code)
+
+
+def ensure_not_running_as_root(fn) -> Any:
+
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        if run_as_root():
+            panic(
+                reason=
+                "This script must be run as non-root user inside docker group",
+                code=ErrorCode.PRECONDITION_FAILED)
+
+        return fn(*args, **kwargs)
+
+    return wrapper
+
+
+def user_id() -> int:
+    return os.geteuid()
+
+
+def group_id() -> int:
+    return os.getegid()
+
+
+def run(cmd: List[str], panic_on_error: bool = True) -> None:
+    shell_cmd = " ".join(cmd)
+    log.debug(f"Running `{shell_cmd}`")
+
+    completed = sp.run(cmd, stdout=sys.stdout, stderr=sys.stderr)
+
+    if completed.returncode != 0:
+        msg = f"Called process `{shell_cmd}` failed with code {completed.returncode}"
+        if panic_on_error:
+            panic(reason=msg, code=ErrorCode.CALLED_PROCESS_ERROR)
+
+        log.warning(msg)
