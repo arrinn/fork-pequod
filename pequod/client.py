51238fdc0bbf56ba4e4315c3548913759f46197e
Roman Lipovsky <roman.lipovsky@gmail.com>
2021-02-04 20:30:07 +0300

    change log message in create cmd

--- pequod/client.py
+++ pequod/client.py
@@ -39,8 +39,7 @@ class Client:
         self._add_group()
         self._add_user()
         self._change_owner()
-        log.info("Container was built successfully, "
-                 "run `docker/client/bin/login` to login")
+        log.info("Container was built successfully")
 
     def remove(self) -> None:
         self._stop()
